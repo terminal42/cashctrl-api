b3fe59264d3f91f98c12a23c7c29ccdc5ac3b188
Andreas Schempp <andreas.schempp@terminal42.ch>
2022-03-09 16:21:44 +0100

    Implemented properties for bookentry

--- src/Entity/OrderBookentry.php
+++ src/Entity/OrderBookentry.php
@@ -24,7 +24,7 @@ namespace Terminal42\CashctrlApi\Entity;
 class OrderBookentry extends AbstractEntity
 {
     protected int $accountId;
-    protected float $amount;
+    protected ?float $amount = null;
     protected int $orderId;
     protected ?int $currencyId = null;
     protected ?float $currencyRate = null;
@@ -33,4 +33,122 @@ class OrderBookentry extends AbstractEntity
     protected ?string $reference = null;
     protected ?int $taxId = null;
     protected ?int $templateId = null;
+
+    public function __construct(int $accountId, int $orderId, int $id = null)
+    {
+        parent::__construct($id);
+
+        $this->accountId = $accountId;
+        $this->orderId = $orderId;
+    }
+
+    public function getAccountId(): int
+    {
+        return $this->accountId;
+    }
+
+    public function setAccountId(int $accountId): self
+    {
+        $this->accountId = $accountId;
+        return $this;
+    }
+
+    public function getAmount(): ?float
+    {
+        return $this->amount;
+    }
+
+    public function setAmount(?float $amount): self
+    {
+        $this->amount = $amount;
+        return $this;
+    }
+
+    public function getOrderId(): int
+    {
+        return $this->orderId;
+    }
+
+    public function setOrderId(int $orderId): self
+    {
+        $this->orderId = $orderId;
+        return $this;
+    }
+
+    public function getCurrencyId(): ?int
+    {
+        return $this->currencyId;
+    }
+
+    public function setCurrencyId(?int $currencyId): self
+    {
+        $this->currencyId = $currencyId;
+        return $this;
+    }
+
+    public function getCurrencyRate(): ?float
+    {
+        return $this->currencyRate;
+    }
+
+    public function setCurrencyRate(?float $currencyRate): self
+    {
+        $this->currencyRate = $currencyRate;
+        return $this;
+    }
+
+    public function getDate(): ?\DateTimeInterface
+    {
+        return $this->date;
+    }
+
+    public function setDate(?\DateTimeInterface $date): self
+    {
+        $this->date = $date;
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(?string $description): self
+    {
+        $this->description = $description;
+        return $this;
+    }
+
+    public function getReference(): ?string
+    {
+        return $this->reference;
+    }
+
+    public function setReference(?string $reference): self
+    {
+        $this->reference = $reference;
+        return $this;
+    }
+
+    public function getTaxId(): ?int
+    {
+        return $this->taxId;
+    }
+
+    public function setTaxId(?int $taxId): self
+    {
+        $this->taxId = $taxId;
+        return $this;
+    }
+
+    public function getTemplateId(): ?int
+    {
+        return $this->templateId;
+    }
+
+    public function setTemplateId(?int $templateId): self
+    {
+        $this->templateId = $templateId;
+        return $this;
+    }
 }
